860bcfa7d568c67772a77e41c83ffa97b17a4ef1
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-22 12:18:02 +0530

    Store_Ui_code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,39 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig
+} from '@angular/core';
 
-import { routes } from './app.routes';
+import {
+  provideRouter
+} from '@angular/router';
 
-export const appConfig: ApplicationConfig = {
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+import {
+  routes
+} from './app.routes';
+
+import {
+  authInterceptor
+} from './interceptors/auth.interceptor';
+
+export const appConfig:
+ApplicationConfig = {
+
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+
+    provideRouter(routes),
+
+    provideHttpClient(
+
+      withInterceptors([
+        authInterceptor
+      ])
+
+    )
+
   ]
+
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+
+import { Navbar }
+from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+
+  imports: [
+    RouterOutlet,
+    Navbar
+    
+],
+
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('flower-shop-ui');
 }
